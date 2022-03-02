b7c66a499b52b859789a1b0d02be29f3be45b0e9
shine_lee <wangyi_lixian@163.com>
2022-03-02 10:41:12 +0800

    添加: 打包相关配置, 版本升级

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const path = require("path");
+const config = {
+  target: "node",
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        include: path.resolve(__dirname, "./src"),
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
+
+module.exports = config;
